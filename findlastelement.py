cd6a219ea025af779c37a9700f40c045898856bb
upload <you@example.com>
2024-11-15 12:28:35 +0000

    findlastelement_modified

--- findlastelement.py
+++ findlastelement.py
@@ -2,3 +2,5 @@
 name=['a', 'b', 'c', 'd']
 '''last_element=name[len(name)-1]'''
 print(f"last name in the list is: {name[len(name) - 1]}")
+last_element_to_upper=name[-1].upper()
+print(f"last element converted to capital case :{last_element_to_upper}")
