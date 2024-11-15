f4ab9ae1222f96ac3e3364195e7658bb5f7503e9
upload <you@example.com>
2024-11-15 14:50:48 +0000

    vaccine

--- vaccine.py
+++ vaccine.py
@@ -5,9 +5,10 @@ if age >= 18:
     taken_first_dose=input("Have you taken your first dose? (Y/N)")
     if taken_first_dose == 'y' or taken_first_dose == 'Y':
         print ("You're eligible for 2nd dose")
-    else:
+    elif taken_first_dose == 'n' or taken_first_dose == 'N':
         print("Please visit your nearesr vaccine center at the earliest")
-
+    else:
+        print("Invalid selection! Please select correct option")
 
 else:
     print("You're minor! Please continue to wear a mask and practice good hygiene") 
