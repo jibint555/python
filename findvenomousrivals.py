91702b3753733f4a9acb38b5e80fccc096c385f7
upload <you@example.com>
2024-11-15 14:29:24 +0000

    findvenomousrivals

---
+++ findvenomousrivals.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+no_of_legs=int(input("Enter the number of legs:\t"))
+
+if no_of_legs == 8:
+   print("OMG, it's a Scorpion or spider")
+elif no_of_legs > 100:
+   print("Oh, It may be a Centipede")
+else:
+   print("mm, It may be a Worm!!!")
+
+
