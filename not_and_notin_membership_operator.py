9308733aac9d6b140f83448bfa4012b9408e1506
upload <you@example.com>
2024-11-15 15:06:12 +0000

    membershipoprator

---
+++ not_and_notin_membership_operator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+vowels=['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+letter=input("Enter a letter: \t")
+if letter in vowels:
+   print("Entered letter is a vowel")
+else:
+    print("Entered leytter is a consonant")
+
