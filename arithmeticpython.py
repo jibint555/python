46890fb3d7c0bf40445d1a437ba1464b06831a3f
Your Name <you@example.com>
2024-11-15 11:21:43 +0000

    new

---
+++ arithmeticpython.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+f_num=int(input("enter the first number:\t"))
+s_num=input("Ente the second number:\t")
+s_num=int(s_num)        
+print("=" * 15)
+print(f"Sum of {f_num} and {s_num}=\t{f_num + s_num}")
+print(f"Difference of {f_num} and {s_num}  =\t {s_num - f_num}")
+print(f"product of {f_num} and {s_num} =\t {f_num * s_num}")
+print(f"Quotient of {f_num} and {s_num} =\t {f_num / s_num}")
