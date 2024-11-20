9ba6aaf8d2e1f8631291976236c8bcb79f735091
upload <you@example.com>
2024-11-20 16:43:26 +0000

    positionalparameters_in_function1

---
+++ positionalparameters_in_function.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def greet_user(username, greet):
+    print(f"{greet} {username}")
+greet_user('Hi', 'Gibin')
---
+++ positionalparameters_in_function1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def greet_user(username, greet):
+    print(f"{greet} {username}")
+greet_user(greet='Hi', username='Gibin')
+
