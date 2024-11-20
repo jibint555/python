3ca053ac044a7a12180aaf2bfd61a90c801a73e0
upload <you@example.com>
2024-11-20 16:23:31 +0000

    function_length.py

--- function_length.py
+++ function_length.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 word=input("Enter your desired string for finding the string: \t")
-length=0
-for _ in word:
-    length+=1
+def length(data):
+   length=0
+   for _ in word:
+      length+=1
+   return length
+for i in range(0, length(word)):
+    print(f"Hello entry ----> {i} \n")
 
-print(length)
+
+
+
